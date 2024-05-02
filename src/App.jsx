c1918215dbc4168804ba81d5d168dcf97a95a908
Abhinav <abhinavke091@gmail.com>
2024-05-02 21:45:41 +0530

    set-storeprovider

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
-import { useEffect, useState } from "react";
+import { StoreProvider } from "./context/StoreContext";
+
 import toast, { Toaster } from "react-hot-toast";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Pricing from "./pages/Pricing/Pricing";
@@ -13,20 +14,22 @@ import ResultPage from "./pages/ResultPage/ResultPage";
 
 function App() {
   return (
-    <div className="app">
-      <div>
-        <Toaster position="bottom-right" reverseOrder={false} />
+    <StoreProvider>
+      <div className="app">
+        <div>
+          <Toaster position="bottom-right" reverseOrder={false} />
+        </div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/certification" element={<Certificate />} />
+          <Route path="/result" element={<ResultPage />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/certification" element={<Certificate />} />
-        <Route path="/result" element={<ResultPage />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </div>
+    </StoreProvider>
   );
 }
 
---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// creating context
+const StoreContext = createContext();
+
+export const StoreProvider = ({ children }) => {
+  const [finalResult, setFinalResult] = useState(null);
+  const navigate = useNavigate();
+  const [isDashboard, setIsDashboard] = useState(false);
+
+
+  const value = {
+    finalResult,
+    setFinalResult
+  };
+
+  return <StoreContext.Provider value={value}>{children}</StoreContext.Provider>;
+};
+
+export const useAuth = () => {
+  return useContext(StoreContext);
+};

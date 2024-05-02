ce6eb947e24d6f62223a1cc581dacc9aa8d25fce
Abhinav <abhinavke091@gmail.com>
2024-05-02 14:37:25 +0530

    linkupload-ui

--- src/components/Tabs/LinkTab/LinkTab.jsx
+++ src/components/Tabs/LinkTab/LinkTab.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import './LinkTab.scss';
-import ICON from '../../../assets/link.svg';
+import React, { useEffect, useState } from "react";
+import "./LinkTab.scss";
+import ICON from "../../../assets/link.svg";
 
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
 const LinkTab = () => {
+  const [link, setLink] = useState("");
+
+  const handleUpload = () => {
+    console.log("link : ", link);
+  };
 
   return (
-    <div className='tab-content'>
-      <div className='i-box'>
-        <img src={ICON} className='icon' alt='icon' />
+    <div className="tab-content">
+      <div className="i-box">
+        <img src={ICON} className="icon" alt="icon" />
         <input
-          type='url'
-          className='input-box'
-          placeholder='https://google.com'
-          // onChange={(e) => setLlink(e.target.value)}
-          // value={lLink}
+          type="url"
+          className="input-box"
+          placeholder="https://google.com"
+          onChange={(e) => setLink(e.target.value)}
+          value={link}
         />
       </div>
-     
-      <button className='create-btn'>
+
+      <button onClick={handleUpload} className="create-btn">
         Upload
       </button>
     </div>
